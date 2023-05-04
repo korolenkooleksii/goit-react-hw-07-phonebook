47d93646b5d0303bbaa3237221043376d3f39b42
Oleksii Korolenko <as_vostok@i.ua>
2023-05-04 11:13:32 +0300

    finish task

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, isAnyOf } from '@reduxjs/toolkit';
 
 import { fetchContacts, deleteContact, addContact } from './operations';
 import { toast } from 'react-toastify';
@@ -13,6 +13,38 @@ const initialState = {
   filter: '',
 };
 
+const extraActions = [fetchContacts, deleteContact, addContact];
+const getActions = type => extraActions.map(action => action[type]);
+const fetchContactsSuccessReducer = ({ contacts }, { payload }) => {
+  contacts.items = payload;
+};
+const addContactSuccessReducer = ({ contacts }, { payload }) => {
+  const duplicate = contacts.items.some(
+    contact => contact.name.toLowerCase() === payload.name.toLowerCase().trim()
+  );
+  if (duplicate) {
+    toast.warn(`${payload.name} is already in contacts.`, {
+      theme: 'colored',
+    });
+  } else {
+    contacts.items.push(payload);
+  }
+};
+const deleteContactSuccessReducer = ({ contacts }, { payload }) => {
+  contacts.items = contacts.items.filter(contact => contact.id !== payload.id);
+};
+const pendingReducer = ({ contacts }) => {
+  contacts.isLoading = true;
+};
+const rejectedReducer = ({ contacts }, { payload }) => {
+  contacts.isLoading = false;
+  contacts.error = payload;
+};
+const fulfilledReducer = ({ contacts }) => {
+  contacts.isLoading = false;
+  contacts.error = null;
+};
+
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
@@ -23,55 +55,13 @@ const contactsSlice = createSlice({
   },
   extraReducers: builder =>
     builder
-      .addCase(fetchContacts.pending, ({ contacts }) => {
-        contacts.isLoading = true;
-      })
-      .addCase(fetchContacts.fulfilled, ({ contacts }, { payload }) => {
-        contacts.isLoading = false;
-        contacts.error = null;
-        contacts.items = payload;
-      })
-      .addCase(fetchContacts.rejected, ({ contacts }, { payload }) => {
-        contacts.isLoading = false;
-        contacts.error = payload;
-      })
-      .addCase(addContact.pending, ({ contacts }) => {
-        contacts.isLoading = true;
-      })
-      .addCase(addContact.fulfilled, ({ contacts }, { payload }) => {
-        contacts.isLoading = false;
-        contacts.error = null;
+      .addCase(fetchContacts.fulfilled, fetchContactsSuccessReducer)
+      .addCase(addContact.fulfilled, addContactSuccessReducer)
+      .addCase(deleteContact.fulfilled, deleteContactSuccessReducer)
 
-        const duplicate = contacts.items.some(
-          contact =>
-            contact.name.toLowerCase() === payload.name.toLowerCase().trim()
-        );
-        if (duplicate) {
-          toast.warn(`${payload.name} is already in contacts.`, {
-            theme: 'colored',
-          });
-        } else {
-          contacts.items.push(payload);
-        }
-      })
-      .addCase(addContact.rejected, ({ contacts }, { payload }) => {
-        contacts.isLoading = false;
-        contacts.error = payload;
-      })
-      .addCase(deleteContact.pending, ({ contacts }) => {
-        contacts.isLoading = true;
-      })
-      .addCase(deleteContact.fulfilled, ({ contacts }, { payload }) => {
-        contacts.isLoading = false;
-        contacts.error = null;
-        contacts.items = contacts.items.filter(
-          contact => contact.id !== payload.id
-        );
-      })
-      .addCase(deleteContact.rejected, ({ contacts }, { payload }) => {
-        contacts.isLoading = false;
-        contacts.error = payload;
-      }),
+      .addMatcher(isAnyOf(...getActions('pending')), pendingReducer)
+      .addMatcher(isAnyOf(...getActions('rejected')), rejectedReducer)
+      .addMatcher(isAnyOf(...getActions('fulfilled')), fulfilledReducer),
 });
 
 export const contactsReducer = contactsSlice.reducer;
